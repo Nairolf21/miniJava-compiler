afe321d2a6214d2ac1127abd1aee799bfb7b652e
yannlefoll125 <yann.lefoll@telecom-bretagne.eu>
2015-12-23 16:11:59 +0100

    Compile: added ParsingError reporting

--- Main/Compile.ml
+++ Main/Compile.ml
@@ -2,6 +2,8 @@ open Parsexpr
 open Lexexpr
 open Error
 
+open Lexing
+
 (* verbose is a boolean that you can use to switch to a verbose output (for example, to dump all the ast) *)
 let execute lexbuf verbose = 
     try
@@ -10,5 +12,9 @@ let execute lexbuf verbose =
         print_string "\n";
     with
         | Error(err, _, _, _) as e -> report_error e
-
+        | Parsexpr.Error -> 
+                let start_p = lexeme_start_p lexbuf in
+                let end_p = lexeme_end_p lexbuf in
+                let e = Error(ParsingError, start_p, end_p, lexbuf) in
+                report_error e
 
