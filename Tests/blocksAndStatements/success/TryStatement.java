37e18ad49db963a74208ed0a2c8ba071c459d72a
Guillaume Villette <guillaume.villette@telecom-bretagne.eu>
2016-01-10 20:19:52 +0100

    test try

---
+++ Tests/blocksAndStatements/success/TryStatement.java
@@ -0,0 +1,36 @@
+public class TryStatement {
+	void foo() {
+		try
+			;
+		catch (Exc e)
+			;
+			
+		try
+			;
+		catch (Excun e)
+			;
+		catch (Excdeux e)
+			;
+			
+		try
+			;
+		finally
+			;
+			
+		try
+			;
+		catch (Exc e)
+			;
+		finally
+			;
+			
+		try
+			;
+		catch (Excun e)
+			;
+		catch (Excdeux e)
+			;
+		finally
+			;
+	}
+}
