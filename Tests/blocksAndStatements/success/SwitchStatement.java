374b1524b5dc43400966e7a91666d1f618dfdfab
Guillaume Villette <guillaume.villette@telecom-bretagne.eu>
2016-01-10 20:05:18 +0100

    test switch

---
+++ Tests/blocksAndStatements/success/SwitchStatement.java
@@ -0,0 +1,78 @@
+public class SwitchStatement {
+	void foo() {
+		switch (i) {}
+		switch (i) {
+			case 1 :
+		}
+		switch (i) {
+			case j :
+		}
+		switch (i) {
+			default :
+		}
+		switch (i) {
+			case 1 : case j :
+		}
+		switch (i) {
+			case 1 :
+				;
+		}
+		switch (i) {
+			case j :
+				;
+		}
+		switch (i) {
+			default :
+				;
+		}
+		switch (i) {
+			case 1 : case j :
+				;
+		}
+		switch (i) {
+			case 1 :
+				;
+			case j :
+		}
+		switch (i) {
+			case j :
+				;
+			case 1 :
+		}
+		switch (i) {
+			case 1 :
+				;
+			default :
+		}
+		switch (i) {
+			case 1 : case j :
+				;
+			default :
+				;
+		}
+				switch (i) {
+			case 1 :
+				;
+			case j :
+				;
+		}
+		switch (i) {
+			case j :
+				;
+			case 1 :
+				;
+		}
+		switch (i) {
+			case 1 :
+				;
+			default :
+				;
+		}
+		switch (i) {
+			case 1 : case j :
+				;
+			default :
+				;
+		}
+	}
+}
