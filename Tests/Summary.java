3d65348bad6426300bd89ade5ff48b64bb705983
yannlefoll125 <yann.lefoll@telecom-bretagne.eu>
2016-01-12 03:13:43 +0100

    added fieldAccess to summary

--- Tests/Summary.java
+++ Tests/Summary.java
@@ -27,6 +27,17 @@ class Summary {
         
         }
         
+    void fieldAccess() {
+        super.x=2;
+        this.x=3;
+        120.x = 3;
+        Cl.super.Cl.x = 4;
+        void.class.x =4; 
+        Cl.this.x = 4;
+        ar[1].x = 4;
+
+        //ClassName.x = 4; Didn't succeed to access class name field
+    }
         void emptyBlock() {}
     
         void oneBlockStatementBlockStatementLocalVariable() {
